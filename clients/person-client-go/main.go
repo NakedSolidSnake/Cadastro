c5a912220485d33b1eeb9bdde692030eef661b1d
solidcris <cssouza@gmail.com>
2022-10-14 19:56:40 -0300

    add print client go

--- clients/person-client-go/main.go
+++ clients/person-client-go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/person-client/routes"
@@ -8,5 +9,6 @@ import (
 
 func main() {
 	routes.LoadRoutes()
+	fmt.Println("Running on port 8009")
 	http.ListenAndServe(":8009", nil)
 }
